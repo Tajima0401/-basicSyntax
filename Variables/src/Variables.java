0d0792b0c0cf4d38d4254d5a629ca1ba33aad3ee
Tajima0401 <s.tajima.cingroup2012@gmail.com>
2024-11-07 12:11:48 +0900

    変数の実装

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -2,19 +2,14 @@
 public class Variables {
 
 	public static void main(String[] args) {
-        // 変数の定義
-        int num1 = 34;           // 整数型
-        double num2 = 0.1;      // 小数型
+       
+        int num1 = 34;
+        double num2 = 0.1;
         
-        // 共通化できる文字列を変数に格納
         String favoriteIntegerMessage = "私の好きな整数は";
         String favoriteDoubleMessage = "私の好きな小数は";
-
-        // num1とnum2の値を出力
         System.out.println(num1);
         System.out.println(num2);
-
-        // メッセージ付きで出力
         System.out.println(favoriteIntegerMessage + num1 + "です。");
         System.out.println(favoriteDoubleMessage + num2 + "です。");
     }
