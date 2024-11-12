44fc5a711c2e32a35d3779828d42a3f7f356ce03
Tajima0401 <s.tajima.cingroup2012@gmail.com>
2024-11-12 09:23:31 +0900

    処理の簡略化

--- src/ComparisonLogicalOperators.java
+++ src/ComparisonLogicalOperators.java
@@ -2,36 +2,21 @@
 public class ComparisonLogicalOperators {
 
 	public static void main(String[] args) {
-
-		int a = 10;
-		int b = 5;
-		if (a > b) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
-
-		boolean isSunny = true;
-		boolean isWarm = true;
-		if (isSunny && isWarm) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
 		
-		int x = 5;
-		int y = 4;
-		if (x >= 0 && y % 2 == 0) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
+     int a = 10;
+     int b = 5;
+     System.out.println(a > b);
 
-		boolean hasPermission = false;
-		if (!hasPermission) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
-	}
+     boolean isSunny = true;
+     boolean isWarm = true;
+     System.out.println(isSunny && isWarm);
+
+     int x = 5;
+     int y = 4;
+     System.out.println(x >= 0 && y % 2 == 0);
+     
+     boolean hasPermission = false;
+     System.out.println(!hasPermission);
+ 
+   }
 }
