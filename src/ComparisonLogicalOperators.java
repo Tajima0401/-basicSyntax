dfe5b340f69c3e8e6f229a1bbf2c8a55a7bd9644
Tajima0401 <s.tajima.cingroup2012@gmail.com>
2024-11-12 22:34:15 +0900

    インデントの修正

--- src/ComparisonLogicalOperators.java
+++ src/ComparisonLogicalOperators.java
@@ -1,21 +1,21 @@
 public class ComparisonLogicalOperators {
 
-	public static void main(String[] args) {
-		
-     int a = 10;
-     int b = 5;
-     System.out.println(a > b);
+    public static void main(String[] args) {
 
-     boolean isSunny = true;
-     boolean isWarm = true;
-     System.out.println(isSunny && isWarm);
+        int a = 10;
+        int b = 5;
+        System.out.println(a > b);
 
-     int x = 5;
-     int y = 4;
-     System.out.println(x >= 0 && y % 2 == 0);
-     
-     boolean hasPermission = false;
-     System.out.println(!hasPermission);
- 
-   }
+        boolean isSunny = true;
+        boolean isWarm = true;
+        System.out.println(isSunny && isWarm);
+
+        int x = 5;
+        int y = 4;
+        System.out.println(x >= 0 && y % 2 == 0);
+
+        boolean hasPermission = false;
+        System.out.println(!hasPermission);
+
+    }
 }
