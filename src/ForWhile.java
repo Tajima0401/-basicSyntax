9403370b9ca5586e09e99fff055353088efd7b0b
Tajima0401 <s.tajima.cingroup2012@gmail.com>
2024-11-15 18:13:43 +0900

    実装内容の修正

--- src/ForWhile.java
+++ src/ForWhile.java
@@ -1,14 +1,28 @@
 
 public class ForWhile {
 	public static void main(String[] args) {
+		System.out.println("== 実装その1 ==");
+		for (int i = 1; i <= 100; i++) {
+			String output = "";
+
+			if (i % 3 == 0)
+				output += "Fizz";
+			if (i % 5 == 0)
+				output += "Buzz";
+
+			if (output.isEmpty()) {
+				System.out.println(i);
+			} else {
+				System.out.println(output);
+			}
+		}
+
+		System.out.println("\n== 実装その2 ==");
 		String[] languages = { "Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript" };
 
 		for (int i = 1; i <= 100; i++) {
-
-			String output = (i % 3 == 0 ? "Fizz" : "") + (i % 5 == 0 ? "Buzz" : "");
-			System.out.println(output.isEmpty() ? i : output);
-
 			String language = languages[(i - 1) % languages.length];
+
 			switch (language) {
 			case "Java":
 				System.out.println("現在学習中の言語はJavaです。");
@@ -18,7 +32,6 @@ public class ForWhile {
 				return;
 			default:
 				System.out.println(language);
-
 			}
 		}
 	}
