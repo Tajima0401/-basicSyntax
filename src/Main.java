f98ce1fa0352b60f9b15d2abb52782d5a4f70e98
Tajima0401 <s.tajima.cingroup2012@gmail.com>
2024-11-18 17:37:14 +0900

    クラスの実装

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		GeekIntroduction geek = new GeekIntroduction();
+
+		System.out.println("=== メソッド① ===");
+		geek.greeting(1, 10);
+
+		System.out.println("=== メソッド② ===");
+		geek.specialSkill(1, 10);
+	}
+}
